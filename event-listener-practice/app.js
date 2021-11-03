6352a303393ca8803fdfd13921a90ba2e0fe3eaf
Daniel Cervantes <cervantesdaniel7@gmail.com>
2021-11-03 10:12:53 -0600

    adding mutliple directories and files that haven't been pushed to git in a while

---
+++ event-listener-practice/app.js
@@ -0,0 +1,7 @@
+// function myFunction() {
+//     console.log("You clicked me!");
+// }
+
+document.getElementById("this-div").addEventListener('click', function () {
+    console.log("You clicked me!");
+})
